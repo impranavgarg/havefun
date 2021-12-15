948660b31ad9568b0b1e59baed27bc8f924ca8e1
Pranav <impranavgarg@gmail.com>
2021-12-16 04:35:12 +0530

    first commit

---
+++ imp/scripti.js
@@ -0,0 +1,31 @@
+const boxes = document.querySelectorAll('.box')
+
+// top header
+const nav = document.querySelector('.nav')
+window.addEventListener('scroll', fixNav)
+
+function fixNav() {
+    if(window.scrollY > nav.offsetHeight + 150) {
+        nav.classList.add('active')
+    } else {
+        nav.classList.remove('active')
+    }
+}
+// top
+window.addEventListener('scroll', checkBoxes)
+
+checkBoxes()
+
+function checkBoxes() {
+    const triggerBottom = window.innerHeight / 5 * 4
+
+    boxes.forEach(box => {
+        const boxTop = box.getBoundingClientRect().top
+
+        if(boxTop < triggerBottom) {
+            box.classList.add('show')
+        } else {
+            box.classList.remove('show')
+        }
+    })
+}
